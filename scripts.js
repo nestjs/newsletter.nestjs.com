e90cce8493a766616648a640ad41f406fe1ee610
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2024-10-22 12:24:05 +0200

    feat: add unsubscribe feature

--- scripts.js
+++ scripts.js
@@ -24,4 +24,41 @@ window.addEventListener('load', function() {
       xhr.send(data);
     });
   }
+
+  // If the "unsubscribe" query string is present, call API to unsubscribe and show message
+  const urlParams = new URLSearchParams(window.location.search);
+  const unsubscribe = urlParams.get('unsubscribe');
+  const unsubscribeDialog = document.querySelector('#unsubscribe-dialog');
+
+  if (unsubscribe) {
+    const xhr = new XMLHttpRequest();
+    const url =
+      'https://nbdggbnqnrevwg6xlex3st3vpe0nyhiq.lambda-url.us-east-2.on.aws/newsletter/unsubscribe'
+    xhr.open('POST', url, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onreadystatechange = function(event) {
+      if (event.target.status === 204) {
+        unsubscribeDialog.classList.remove('invisible');
+      } else if (event.target.status === 404) {
+        // Ignore not found errors
+        return;
+      } else {
+        const dialogHeader = unsubscribeDialog.querySelector('.dialog-header h2');
+        const dialogContent = unsubscribeDialog.querySelector('.dialog-content p');
+
+        dialogHeader.innerHTML = 'Something went wrong';
+        dialogContent.innerHTML = "We couldn't unsubscribe your email. You may already be unsubscribed, or the unsubscribe link might be invalid. Please try again later, and if the issue persists, contact us.";
+        unsubscribeDialog.classList.remove('invisible');
+      }
+    };
+    const data = JSON.stringify({ token: unsubscribe });
+    xhr.send(data);
+  }
+
+  const unsubscribeDialogClose = document.querySelector('#close-dialog');
+  if (unsubscribeDialogClose) {
+    unsubscribeDialogClose.addEventListener('click', function() {
+      unsubscribeDialog.classList.add('invisible');
+    });
+  }
 });
